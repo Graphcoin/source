a3687a0264cbc01bf2cfc053ac4f74a27cefda43
MotoAcidic <travisfinch01@gmail.com>
2019-01-26 10:05:29 -0500

    Version / Protocol Bump

--- src/main.cpp
+++ src/main.cpp
@@ -6600,12 +6600,13 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 int ActiveProtocol()
 {
 	// SPORK_14 is used for 70913 (v3.1.0+)
-	if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
-		return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
+	//if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
+		//return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
 
 	// SPORK_15 was used for 70912 (v3.0.5+), commented out now.
-	//if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
-	//        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
+	// Spork_15 will be used to kick old wallets off from staking exploit
+	if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
+	        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
 
 	return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
